d8c6e363f2e343d04ac9890217d92aa9290a1ae0
mohammadjavad_pc <m_javad_96@yahoo.com>
2020-10-20 22:32:31 +0330

    candle chart

--- financial_api.py
+++ financial_api.py
@@ -3,15 +3,84 @@ import csv
 import json
 import pandas as pd 
 import numpy as np 
+
 from matplotlib import pyplot as plt
 from matplotlib import dates as mpl_dates
+import matplotlib.ticker as mticker
+import mplfinance as mplf
+
+
 import key1
 
 key = key1.key()
 
-company = 'IBM'
+company = 'twtr'
 outputsize = 'compact'
 response = requests.get(f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={company}&outputsize={outputsize}&apikey={key}')
 
 res = json.loads(response.text)
 df = pd.DataFrame(res)
+
+df.dropna(axis='index', how ='all', subset=['Time Series (Daily)'], inplace=True)
+
+df.index.rename('Date', inplace=True)
+df.dropna(axis='columns', how='all', inplace=True)
+
+df['open'] = (df['Time Series (Daily)'].apply(lambda x: x.get('1. open', 0))).astype(float)
+df['high'] = (df['Time Series (Daily)'].apply(lambda x: x.get('2. high', 0))).astype(float)
+df['low'] = (df['Time Series (Daily)'].apply(lambda x: x.get('3. low', 0))).astype(float)
+df['Close'] = (df['Time Series (Daily)'].apply(lambda x: x.get('4. close', 0))).astype(float)
+df['adjusted close'] = (df['Time Series (Daily)'].apply(lambda x: x.get('5. adjusted close', 0))).astype(float)
+df['Valume'] = (df['Time Series (Daily)'].apply(lambda x: x.get('6. volume', 0))).astype(float)
+df['dividend amount'] = (df['Time Series (Daily)'].apply(lambda x: x.get('7. dividend amount', 0))).astype(float)
+df['split coefficient'] = (df['Time Series (Daily)'].apply(lambda x: x.get('8. split coefficient', 0))).astype(float)
+
+df.drop(columns=['Time Series (Daily)'], inplace=True)
+
+df.index = pd.to_datetime(df.index)
+
+buying_point = df.loc['2020-07-07']['Close'].min()
+buying_date = df.loc['2020-07-07'].index
+
+# ploting
+fig = plt.figure()
+ax=plt.subplot2grid((1,1), (0,0))
+ax.fill_between(df.index, df['Close'], buying_point,
+            where=(df['Close'] > buying_point),
+            alpha=0.5, facecolor='g',
+            interpolate=True,
+            label='Profect'
+             )
+
+ax.fill_between(df.index, df['Close'], buying_point,
+            where=(df['Close'] < buying_point),
+            alpha=0.5, facecolor='r',
+            interpolate=True,
+            label='Loss'
+             )
+
+
+ax.plot_date(df.index, df['Close'], '-', label='Price')
+ax.axvline(buying_date , color='#786763')
+ax.axhline(buying_point, color ='g')
+
+for label in ax.xaxis.get_ticklabels():
+    label.set_rotation(30)
+
+ax.spines['left'].set_color('c')
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+
+ax.tick_params(axis='x', color='#786763')
+
+date_format = mpl_dates.DateFormatter('%b, %d %Y')
+plt.gca().xaxis.set_major_formatter(date_format)
+plt.gcf().autofmt_xdate()
+
+
+# ax.grid(True)#, color='g')
+plt.legend()
+plt.xlabel('Date')
+plt.ylabel('Price (USD)')
+plt.title(company)
+plt.show()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-for label in ax.xaxis.get_ticklabels():
-    label.set_rotation(30)
-date_format = mpl_dates.DateFormatter('%b, %d %Y')
-plt.gca().xaxis.set_major_formatter(date_format)
-=======
-plt.xlabel('Date')
->>>>>>> 0c9117a0f750e2c234becc7d830c1a5d67600c3c
+import matplotlib.ticker as mticker
+import mplfinance as mplf
